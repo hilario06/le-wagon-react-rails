1b36bd1b6db9f38da3c2f445950ff9a5a88cf953
luis <hilario.huaman.6@gmail.com>
2022-02-14 16:34:39 -0500

    solution

--- React Tooling/02-Manipulating-Strings/lib/01_dash_insertion.js
+++ React Tooling/02-Manipulating-Strings/lib/01_dash_insertion.js
@@ -1,6 +1,24 @@
 function insertDash(word) {
   // TODO: implement the method and return word with dashes
-  return null;
+  const vocales = ['a', 'e', 'i', 'o', 'u', ' '];
+  let contador = 0;
+  let concat = '';
+  word.split('').forEach((valor) => {
+    if (vocales.includes(valor.toLowerCase())) {
+      concat += valor;
+      contador = 0;
+    } else {
+      contador += 1;
+      if (contador === 2) {
+        concat += `-${valor}`;
+        contador = 1;
+      } else {
+        concat += valor;
+      }
+    }
+  });
+  return concat;
 }
 
 module.exports = insertDash;
+console.log(insertDash("Internationalization"));
--- React Tooling/02-Manipulating-Strings/lib/02_counting_words.js
+++ React Tooling/02-Manipulating-Strings/lib/02_counting_words.js
@@ -1,5 +1,12 @@
 function occurrences(text, word) {
-  return null;
+  let count = 0;
+  text.split(' ').forEach((element) => {
+    count += element.toLowerCase() === word.toLowerCase() ? 1 : 0;
+  });
+  return count;
 }
 
 module.exports = occurrences;
+console.log(occurrences("a fat cat sat on a mat and ate a fat rat", "at"));
+console.log(occurrences("", "hello"));
+console.log(occurrences("a fat cat sat on a mat and ate a fat rat", "cat"));
